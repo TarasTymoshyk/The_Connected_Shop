b3a04e509b00abdaa7482c23c50bdec8e3ea11ce
tes2you <2impulse0@gmail.com>
2026-01-12 21:54:13 +0200

    new page is added

--- tests/POM.spec.ts
+++ tests/POM.spec.ts
@@ -1,38 +1,38 @@
-import { expect, test } from '@playwright/test'
-import { HomePage } from '../pages/homepage'
-import { Header } from '../pages/header'
-import { Footer } from '../pages/footer'
-import { Search } from '../pages/Search'
-import { SearchValidation } from '../pages/SearchValidation'
+// import { expect, test } from '@playwright/test'
+// import { HomePage } from '../pages/homepage'
+// import { Header } from '../pages/header'
+// import { Footer } from '../pages/footer'
+// import { Search } from '../pages/Search'
+// import { SearchValidation } from '../pages/SearchValidation'
 
-test.describe('Check elements of homepage', () => {
-    let homepage: HomePage
-    let header: Header
-    let footer: Footer
-    let search: Search
-    let search_test: SearchValidation
-    test.beforeEach(async ({ page }) => {
-        homepage = new HomePage(page)
-        header = new Header(page)
-        footer = new Footer(page)
-        search = new Search(page)
-        search_test = new SearchValidation(page)
-        await homepage.openHomePage()
-    })
-    test('Check_UI_elements', async ({ page }) => {
-        await header.checkLogo_link()
-        await header.mainLogo()
-        await header.cartLink()
-        await footer.ourStoryLink()
-        await footer.bbb()
-        await search.checkSearchIcon()
-        await search.checkSearchPlaceholder()
-        await search.checkSearchIcon()
-    })
-    test('Search_validation', async ({ page }) => {
-        await search_test.searchData("Smart")
-        await search_test.verifySearchResults()
-        await search_test.searchData("adfaksdfnakljsdnf")
-        await search_test.verifyNoResults()
-    })
-})
+// test.describe('Check elements of homepage', () => {
+//     let homepage: HomePage
+//     let header: Header
+//     let footer: Footer
+//     let search: Search
+//     let search_test: SearchValidation
+//     test.beforeEach(async ({ page }) => {
+//         homepage = new HomePage(page)
+//         header = new Header(page)
+//         footer = new Footer(page)
+//         search = new Search(page)
+//         search_test = new SearchValidation(page)
+//         await homepage.openHomePage()
+//     })
+//     test('Check_UI_elements', async ({ page }) => {
+//         await header.checkLogo_link()
+//         await header.mainLogo()
+//         await header.cartLink()
+//         await footer.ourStoryLink()
+//         await footer.bbb()
+//         await search.checkSearchIcon()
+//         await search.checkSearchPlaceholder()
+//         await search.checkSearchIcon()
+//     })
+//     test('Search_validation', async ({ page }) => {
+//         await search_test.searchData("Smart")
+//         await search_test.verifySearchResults()
+//         await search_test.searchData("adfaksdfnakljsdnf")
+//         await search_test.verifyNoResults()
+//     })
+// })
--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,70 +1,70 @@
-import { expect, test } from '@playwright/test'
-test.describe('Check elements of homepage', () => {
-    test('homepage test', async ({ page }) => {
-        await page.goto('/')
-        await expect(page).toHaveURL('/')
-        await expect(page).toHaveTitle('The Connected Shop - Smart Locks, Smart Sensors, Smart Home & Office')
-    })
+// import { expect, test } from '@playwright/test'
+// test.describe('Check elements of homepage', () => {
+//     test('homepage test', async ({ page }) => {
+//         await page.goto('/')
+//         await expect(page).toHaveURL('/')
+//         await expect(page).toHaveTitle('The Connected Shop - Smart Locks, Smart Sensors, Smart Home & Office')
+//     })
 
-    test('main_logo_verification', async ({ page }) => {
-        await page.goto('/')
-        const logo = page.locator('a.header__heading-link').nth(0)
-        const logoImage = logo.locator('img.header__heading-logo')
-        await expect(logo).toBeVisible({ timeout: 3000 })
-        await expect(logo).toHaveAttribute('href', '/')
-        await expect(logoImage).toBeVisible()
-        await expect(logoImage).toHaveAttribute('src', '//theconnectedshop.com/cdn/shop/files/The_Connected_Shop_Logo_3468ca3e-4c94-4a38-b086-2fca61a54d36.png?v=1713460187&width=500')
-        await expect(logoImage).toHaveAttribute('width', '180')
-        await expect(logoImage).toHaveAttribute('height', '90.0')
-    })
+//     test('main_logo_verification', async ({ page }) => {
+//         await page.goto('/')
+//         const logo = page.locator('a.header__heading-link').nth(0)
+//         const logoImage = logo.locator('img.header__heading-logo')
+//         await expect(logo).toBeVisible({ timeout: 3000 })
+//         await expect(logo).toHaveAttribute('href', '/')
+//         await expect(logoImage).toBeVisible()
+//         await expect(logoImage).toHaveAttribute('src', '//theconnectedshop.com/cdn/shop/files/The_Connected_Shop_Logo_3468ca3e-4c94-4a38-b086-2fca61a54d36.png?v=1713460187&width=500')
+//         await expect(logoImage).toHaveAttribute('width', '180')
+//         await expect(logoImage).toHaveAttribute('height', '90.0')
+//     })
 
-    test('search_area_ui_verification', async ({ page }) => {
-        await page.goto('/')
-        const search_area = page.locator('form.search').first()
-        await expect(search_area).toBeVisible()
-        await expect(search_area).toHaveAttribute('action', /search/)
+//     test('search_area_ui_verification', async ({ page }) => {
+//         await page.goto('/')
+//         const search_area = page.locator('form.search').first()
+//         await expect(search_area).toBeVisible()
+//         await expect(search_area).toHaveAttribute('action', /search/)
 
-        const search_placeholder = page.locator('#Search-In-Inline')
-        await expect(search_placeholder).toBeVisible()
-        await expect(search_placeholder).toHaveAttribute('placeholder', 'Search')
+//         const search_placeholder = page.locator('#Search-In-Inline')
+//         await expect(search_placeholder).toBeVisible()
+//         await expect(search_placeholder).toHaveAttribute('placeholder', 'Search')
 
-        const search_icon = page.locator('use[href="#icon-search"], use[*|href="#icon-search"]')
-        await expect(search_icon.first()).toBeVisible()
+//         const search_icon = page.locator('use[href="#icon-search"], use[*|href="#icon-search"]')
+//         await expect(search_icon.first()).toBeVisible()
 
-        const search_autocomplete = page.locator('#Search-In-Inline')
-        await expect(search_autocomplete).toBeVisible()
-        await expect(search_autocomplete).toHaveAttribute('autocomplete', 'off')
-    })
+//         const search_autocomplete = page.locator('#Search-In-Inline')
+//         await expect(search_autocomplete).toBeVisible()
+//         await expect(search_autocomplete).toHaveAttribute('autocomplete', 'off')
+//     })
 
-    test('customer support', async ({ page }) => {
-        await page.goto('/')
-        const cell_redirection = page.locator('a[href^="tel:"]').first()
-        await expect(cell_redirection).toBeHidden()
-        await expect(cell_redirection).toHaveAttribute('href', 'tel:(305) 330-3424')
-    })
+//     test('customer support', async ({ page }) => {
+//         await page.goto('/')
+//         const cell_redirection = page.locator('a[href^="tel:"]').first()
+//         await expect(cell_redirection).toBeHidden()
+//         await expect(cell_redirection).toHaveAttribute('href', 'tel:(305) 330-3424')
+//     })
 
-    test('account_icon', async ({ page }) => {
-        await page.goto('/')
-        const account_icon = page.locator('a.header__icon.header__icon--account').last()
-        await expect(account_icon).toBeVisible({ timeout: 3000 })
-        // await expect(account_icon).toHaveAttribute('href', 'https://theconnectedshop.com/customer_authentication/redirect?locale=en&region_country=UA')
-    })
+//     test('account_icon', async ({ page }) => {
+//         await page.goto('/')
+//         const account_icon = page.locator('a.header__icon.header__icon--account').last()
+//         await expect(account_icon).toBeVisible({ timeout: 3000 })
+//         // await expect(account_icon).toHaveAttribute('href', 'https://theconnectedshop.com/customer_authentication/redirect?locale=en&region_country=UA')
+//     })
 
-    test('cart_icon', async ({ page }) => {
-        await page.goto('/')
-        const cart_icon = page.locator('a.header__icon--cart').last()
-        await expect(cart_icon).toBeVisible()
-        await expect(cart_icon).toHaveAttribute('href', '/cart')
-    })
+//     test('cart_icon', async ({ page }) => {
+//         await page.goto('/')
+//         const cart_icon = page.locator('a.header__icon--cart').last()
+//         await expect(cart_icon).toBeVisible()
+//         await expect(cart_icon).toHaveAttribute('href', '/cart')
+//     })
 
-    test('footer_OurStory', async ({ page }) => {
-        await page.goto('/')
-        await expect(page.getByRole('link', { name: 'About us' })).toHaveAttribute('href', '/pages/about-us')
-    })
+//     test('footer_OurStory', async ({ page }) => {
+//         await page.goto('/')
+//         await expect(page.getByRole('link', { name: 'About us' })).toHaveAttribute('href', '/pages/about-us')
+//     })
 
-    test('footer_BBBRating', async({ page })=> {
-        await page.goto('/') // main page
-        const BBB = page.locator('img[src*="bbb-92025167.png"]')
-        await expect(BBB).toHaveAttribute('src', 'https://seal-seflorida.bbb.org/seals/black-seal-293-61-bbb-92025167.png')
-    })
-})
+//     test('footer_BBBRating', async({ page })=> {
+//         await page.goto('/') // main page
+//         const BBB = page.locator('img[src*="bbb-92025167.png"]')
+//         await expect(BBB).toHaveAttribute('src', 'https://seal-seflorida.bbb.org/seals/black-seal-293-61-bbb-92025167.png')
+//     })
+// })
